048341abf582ab7ed2e30b3fbd3889d20ed03f17
JC Gurango <jc@jcgurango.com>
2025-03-09 18:24:20 +0800

    Implement server stats

--- src/lib/components/Settings/Stats.svelte
+++ src/lib/components/Settings/Stats.svelte
@@ -1,6 +1,65 @@
 <script lang="ts">
-  import { totalStats } from '$lib/settings';
-  import { AccordionItem, Label } from 'flowbite-svelte';
+  import { browser } from '$app/environment';
+  import { totalStats, volumes } from '$lib/settings';
+  import { promptConfirmation, showSnackbar } from '$lib/util';
+  import { AccordionItem, Button, Input, Label } from 'flowbite-svelte';
+  import { onMount } from 'svelte';
+
+  let syncId: string = '';
+
+  function makeid(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
+  onMount(() => {
+    console.log(localStorage.getItem('syncId'));
+    syncId = localStorage.getItem('syncId') || makeid(12);
+  });
+
+  async function onUploadVolumeData() {
+    try {
+      showSnackbar('Uploading volume data....', 0);
+      await fetch('/volume-data/' + syncId, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: localStorage.getItem('volumes') || '{}'
+      });
+      showSnackbar('Volume data uploaded');
+    } catch (err) {
+      showSnackbar('Error: ' + (err as any).message);
+    }
+  }
+
+  async function onDownloadVolumeData() {
+    try {
+      showSnackbar('Downloading volume data....', 0);
+      const response = await fetch('/volume-data/' + syncId);
+      const downloaded = await response.json();
+
+      volumes.update((prev) => ({
+        ...prev,
+        ...downloaded
+      }));
+
+      showSnackbar('Volume data downloaded');
+    } catch (err) {
+      showSnackbar('Error: ' + (err as any).message);
+    }
+  }
+
+  $: if (browser && syncId) {
+    localStorage.setItem('syncId', syncId);
+  }
 </script>
 
 <AccordionItem>
@@ -11,4 +70,25 @@
     <p>Characters read: {$totalStats?.charsRead || 0}</p>
     <p>Minutes read: {$totalStats?.minutesRead || 0}</p>
   </div>
+  <form>
+    <div>
+      <Label for="sync_id" class="mb-2">Sync ID (Used for syncing w/ the server)</Label>
+      <Input type="text" id="sync_id" placeholder="Any string" bind:value={syncId} required />
+    </div>
+    <div class="flex-col gap-2-flex">
+      <Button
+        color="dark"
+        on:click={() => promptConfirmation('Upload volume data?', onUploadVolumeData)}
+      >
+        Upload volume data
+      </Button>
+      <Button
+        color="alternative"
+        on:click={() =>
+          promptConfirmation('Download and overwrite volume data?', onDownloadVolumeData)}
+      >
+        Download volume data
+      </Button>
+    </div>
+  </form>
 </AccordionItem>
